e1b250260f15ca2173ea9609d47a8491169e17d1
Freek Hoekstra <freek.hoekstra@cohesion.nl>
2024-02-19 13:14:37 +0100

    Add route for step 1 and render router-outlet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ModelAndColorComponent } from './model-and-color/model-and-color.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'step-1', component: ModelAndColorComponent},
+    {path: '**', redirectTo: '/step-1'}
+];
