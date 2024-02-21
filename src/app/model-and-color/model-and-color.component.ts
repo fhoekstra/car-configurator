066b63e819ef27bd73558b5903f706c84c5fda13
Freek Hoekstra <freek.hoekstra@cohesion.nl>
2024-02-21 18:58:38 +0100

    Attach Model and Color Component to API

--- src/app/model-and-color/model-and-color.component.ts
+++ src/app/model-and-color/model-and-color.component.ts
@@ -1,30 +1,56 @@
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Component, computed, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { FormsModule } from '@angular/forms';
+import { tap } from 'rxjs';
 
 type Model = {
   description: string
+  code: string
+  colors: Color[]
+}
+
+type Color = {
+  code: string
+  description: string
+  price: number
 }
 
 @Component({
   selector: 'app-model-and-color',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgFor, FormsModule],
   templateUrl: './model-and-color.component.html',
   styleUrl: './model-and-color.component.scss'
 })
 export class ModelAndColorComponent {
 
-  models$ = new Observable();
+  private _selectedModelCode = signal<string>("");
+  private _selectedColorCode = signal<string>("");
+  availableModels = toSignal(this.http.get<Model[]>('http://127.0.0.1:8777/models'), {initialValue: []});
+  
+  availableColors = computed(
+    () => this.availableModels()
+    .filter(m => m.code == this._selectedModelCode())[0]
+    ?.colors
+  )
+
   constructor(private http: HttpClient) {}
 
-  ngOnInit(){
-    this.models$ = this.http.get<Model[]>('http://127.0.0.1:8777/models').pipe(tap(
-      value => {
-        console.log(value)
-      }
-    ))
+  set SelectedModelCode(modelCode: string) {
+    this._selectedModelCode.set(modelCode);
+  }
+
+  get SelectedModelCode(){
+    return this._selectedModelCode();
+  }
+
+  set SelectedColorCode(colorCode: string) {
+    this._selectedColorCode.set(colorCode);
+  }
+
+  get SelectedColorCode(){
+    return this._selectedColorCode();
   }
 }
