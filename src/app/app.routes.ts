c6c815a01a1150458b6325982d37fb999d6426b0
Freek Hoekstra <freek.hoekstra@cohesion.nl>
2024-02-22 17:53:12 +0100

    Add router guard for step 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,25 @@ import { Routes } from '@angular/router';
 import { ModelAndColorComponent } from './model-and-color/model-and-color.component';
 import { OptionsComponent } from './options/options.component';
 import { SummaryComponent } from './summary/summary.component';
+import { inject } from '@angular/core';
+import { ModelService } from './state/model.service';
+import { ColorService } from './state/color.service';
+import { combineLatest, map } from 'rxjs';
+
+export const canActivateOptions = () => {
+  return combineLatest([
+    inject(ModelService).modelCode$,
+    inject(ColorService).colorCode$,
+  ]).pipe(map(([a, b]) => a !== '' && b !== ''));
+};
 
 export const routes: Routes = [
   { path: 'step-1', component: ModelAndColorComponent },
-  { path: 'step-2', component: OptionsComponent },
+  {
+    path: 'step-2',
+    component: OptionsComponent,
+    canActivate: [canActivateOptions],
+  },
   { path: 'step-3', component: SummaryComponent },
   { path: '', redirectTo: '/step-1', pathMatch: 'full' },
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { canActivateOptions } from '../app.routes';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent { }
+export class NavbarComponent {
+  canActivateStep2 = toSignal(canActivateOptions());
+}
