eceb8d92515c461ca4200afc96366854bf84124e
Freek Hoekstra <freek.hoekstra@cohesion.nl>
2024-02-22 15:51:09 +0100

    feat: Add navbar, routes and pagination

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import {Component} from '@angular/core';
-import {AsyncPipe, JsonPipe} from '@angular/common';
+import { Component } from '@angular/core';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, RouterOutlet],
-  templateUrl: './app.component.html'
+  imports: [AsyncPipe, JsonPipe, RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   name = 'Angular';
-
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { ModelAndColorComponent } from './model-and-color/model-and-color.component';
+import { OptionsComponent } from './options/options.component';
+import { SummaryComponent } from './summary/summary.component';
 
 export const routes: Routes = [
-    {path: 'step-1', component: ModelAndColorComponent},
-    {path: '', redirectTo: '/step-1', pathMatch: 'full'}
+  { path: 'step-1', component: ModelAndColorComponent },
+  { path: 'step-2', component: OptionsComponent },
+  { path: 'step-3', component: SummaryComponent },
+  { path: '', redirectTo: '/step-1', pathMatch: 'full' },
 ];
