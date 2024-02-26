5f8ea06626be59b95a84216a1f994fddf6d15232
Freek Hoekstra <freek.hoekstra@cohesion.nl>
2024-02-26 11:03:58 +0100

    fix: make step-2 remember state

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ImageComponent } from '../image/image.component';
 import { OptionsService } from '../state/options.service';
@@ -14,9 +14,9 @@ import { Config } from '../types';
   styleUrl: './options.component.scss',
 })
 export class OptionsComponent {
-  private _selectedConfigId = signal<number>(-1);
-  private _isTowHitchSelected = signal<boolean>(false);
-  private _isYokeSelected = signal<boolean>(false);
+  selectedConfigId = this.optionsState.configId;
+  isTowHitchSelected = this.optionsState.includeTow;
+  isYokeSelected = this.optionsState.includeYoke;
 
   availableOptions = this.fetchOptions.availableOptions;
   availableConfigs = computed(() =>
@@ -25,7 +25,7 @@ export class OptionsComponent {
 
   selectedConfig = computed<Config>(() => {
     const selectedConfig = this.availableConfigs()?.filter(
-      (c) => c.id == this._selectedConfigId(),
+      (c) => c.id == this.selectedConfigId(),
     );
     if (!selectedConfig)
       return {
@@ -47,31 +47,4 @@ export class OptionsComponent {
     private fetchOptions: FetchOptionsService,
     private optionsState: OptionsService,
   ) { }
-
-  set SelectedConfigId(configId: number) {
-    this._selectedConfigId.set(configId);
-    this.optionsState.saveConfigId(configId);
-  }
-
-  get SelectedConfigId() {
-    return this._selectedConfigId();
-  }
-
-  set IsYokeSelected(selected: boolean) {
-    this._isYokeSelected.set(selected);
-    this.optionsState.saveIncludeYoke(selected);
-  }
-
-  get IsYokeSelected() {
-    return this._isYokeSelected();
-  }
-
-  set IsTowHitchSelected(selected: boolean) {
-    this._isTowHitchSelected.set(selected);
-    this.optionsState.saveIncludeTow(selected);
-  }
-
-  get IsTowHitchSelected() {
-    return this._isTowHitchSelected();
-  }
 }
